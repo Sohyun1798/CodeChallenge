e2354d77934be634b8786ef7e51c7221d96ec932
Sohyun Park <polivia1798@gmail.com>
2020-03-24 18:12:14 +0000

    first commit

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int factorial(int n){
+
+    if(n == 0) return 1;
+
+    else return n*factorial(n-1);
+
+}
+
+int main(){
+
+    int n = 5;
+
+    printf("%d", factorial(n));
+
+    return 0;
+}
