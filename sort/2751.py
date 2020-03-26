c9d771a08aa7d395225164b9308c5f60b7970e97
Sohyun Park <polivia1798@gmail.com>
2020-03-26 11:14:47 +0000

    complete

---
+++ sort/2751.py
@@ -0,0 +1,40 @@
+from heapq import heappush, heappop
+
+def get_input():
+
+    n = int(input())
+    input_list = []
+    
+    for i in range(n):
+        input_list.append(int(input()))
+
+    re_dup = list(set(input_list)) #keep in mind set and dict
+
+    return re_dup
+
+def heap_sort(input_list):
+
+    heap = []
+
+    for item in input_list:
+        heappush(heap, item)
+
+    ordered = []
+
+    while heap:
+        ordered.append(heappop(heap))
+
+    return ordered
+
+def main():
+
+    input_list = get_input()
+    result = heap_sort(input_list)
+    
+    for i in range(len(result)):
+        print(result[i])
+
+if __name__ == "__main__":
+    main()
+    
+
