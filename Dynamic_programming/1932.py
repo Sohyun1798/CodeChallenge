424eecb7b2325afdf842dd189efd28e1ea081d2a
Sohyun Park <polivia1798@gmail.com>
2020-03-31 08:42:49 +0100

    input func

---
+++ Dynamic_programming/1932.py
@@ -0,0 +1,13 @@
+def get_input():
+
+    a = []
+
+    n = int(input())
+
+    for i in range(n):
+        input_tri = [list(input()) for _ in range(i)]
+
+    return n, input_tri
+
+def dynamic_program():
+    pass
