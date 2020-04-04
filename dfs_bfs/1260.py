424cbc8aaa1e117c103916839183145fd407322e
Sohyun Park <polivia1798@gmail.com>
2020-04-04 23:31:33 +0100

    input function

---
+++ dfs_bfs/1260.py
@@ -0,0 +1,11 @@
+def get_input():
+
+    n = list(map(int, input().split()))
+    line = []
+    
+    for i in range(n[1]):
+        line.append(list(map(int, input().split())))
+
+    return n, line
+
+
