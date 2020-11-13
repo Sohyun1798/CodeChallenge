e34ece3886124ecc7f1abf58fc89b247cf783fde
Sohyun Park <polivia1798@gmail.com>
2020-11-13 21:20:09 +0900

    fail:(

--- Dynamic_programming/14002.py
+++ Dynamic_programming/14002.py
@@ -1,9 +1,34 @@
 def get_input():
 
     n = int(input())
-    input_list = []
 
-    for _ in range(n):
-        input_list.append(list(map(int, input().split())))
+    input_list = []
+    input_list.extend(list(map(int, input().split())))
     
-    return n, input_list
+    return input_list
+
+def dp_14002():
+
+    input_list = get_input()
+    temp = []
+
+    for i in range(len(input_list)):
+
+        if i == 0:
+            temp.append(input_list[i])
+            j = 0
+
+        else:
+            if temp[j] < input_list[i]:
+                temp.append(input_list[i])
+                j += 1
+
+    ans = len(temp)
+
+    return ans, temp
+
+print(dp_14002())
+
+
+
+
