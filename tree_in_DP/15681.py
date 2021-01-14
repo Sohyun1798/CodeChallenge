0039c1c597ab2f317c5a0ba485c00f404264b2cc
Sohyun Park <polivia1798@gmail.com>
2021-01-14 19:58:00 +0900

    15681 failed :(

--- tree_in_DP/15681.py
+++ tree_in_DP/15681.py
@@ -3,8 +3,7 @@ def get_input():
     N, R, Q = map(int, input().split())
     node_graph = {i:[] for i in range(1, N+1)}
 
-    query = [0]*(Q+1)
-    line = []
+    query = [0]*Q
 
     for _ in range(N-1):
         left, right = map(int, input().split())
@@ -12,10 +11,34 @@ def get_input():
         node_graph[right] += [left]
       
     for j in range(Q):
-        query[j+1] = int(input())
+        query[j] = int(input())
 
-    return N, R, Q, line, query
+    return N, R, Q, node_graph, query
 
 N, R, Q, line, query = get_input()
 print(line)
+
+answer = []
+
+for ele in query:
+
+    stack = [ele]
+    result = []
+
+    while stack:
+
+        node = stack.pop()
+        result += [node]
+
+        for child in line[node]:
+
+            if child in result or child in stack:
+                continue
+            else: stack += [child]
+
+    answer.append(len(result))
+
+print(answer)
+
+
 
