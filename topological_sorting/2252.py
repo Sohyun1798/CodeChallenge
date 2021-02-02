db4a3f91556dd8e884351f082331679ff9a60a94
Sohyun Park <polivia1798@gmail.com>
2021-02-02 21:07:05 +0900

    complete

--- topological_sorting/2252.py
+++ topological_sorting/2252.py
@@ -14,7 +14,20 @@ def TS_2252():
 
     topolSort = [i for i in range(1, N+1)]
 
+    for i in range(M):
+
+        A = sList[i][0]
+        B = sList[i][1]
+        Aidx = topolSort.index(A)
+        Bidx = topolSort.index(B)
+
+        if Aidx > Bidx:
+            topolSort[Bidx] = A
+            topolSort[Aidx] = B
+
     return topolSort
+
+print(TS_2252())
 
 
 
