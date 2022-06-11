69f72ead9c146339a36dadcde5e4f9fc3090f806
Sohyun Park <polivia1798@gmail.com>
2022-06-10 19:54:49 -0600

    nqueen explanation

---
+++ backtracking/leetcode_51.py
@@ -0,0 +1,10 @@
+#https://jayce-with.tistory.com/17
+#https://bumbums.tistory.com/3
+
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        
