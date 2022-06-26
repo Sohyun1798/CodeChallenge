2a10ec6d100c08ad1cd0c8d0effc8ed6cef14a0b
Sohyun Park <polivia1798@gmail.com>
2022-06-25 21:56:27 -0600

    Longest Common Prefix

--- others/leetcode_14.py
+++ others/leetcode_14.py
@@ -1,20 +1,33 @@
+from curses.ascii import SO
+
+
 class Solution(object):
-    def longestCommonPrefix(self, strs):
+    def longestCommonPrefix(self,strs):
         """
         :type strs: List[str]
         :rtype: str
         """
-        temp = []
-        
-        for i in range(1,len(strs)):
+        answer = ""
+        criterion = min(strs, key=len)
 
-            for j in range(len(min(strs, key=len))):
+        if len(strs) == 1 or criterion=="":
 
-                criterion = strs[0][j]
+            return criterion
 
-                if strs[i][j] != criterion:
+        else:
+            
+            for j in range(len(criterion)):
+                for i in range(1, len(strs)):
+                    temp = strs[0][j]
+                   
+                    if strs[i][j] != temp:
+                        temp = ""
+                        break
+                    
+                if temp != "":
+                    answer += temp
+
+                else:
                     break
-
-                
-
-
+                    
+            return answer
